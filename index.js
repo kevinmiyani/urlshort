156f17d5216a699830a7a804344559e4d986adb5
Kevin Miyani <67430181+kevinmiyani@users.noreply.github.com>
2024-05-08 20:16:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ const port = process.env.PORT;
 
 Connection(username, password);
 
+// app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "public"));
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", async (req, res) => {
