7d3fa09b1c46ceb4850334cb0155a34690b6b841
Kevin Miyani <67430181+kevinmiyani@users.noreply.github.com>
2024-05-15 19:51:43 +0530

    Update index.js

--- helper/index.js
+++ helper/index.js
@@ -9,4 +9,28 @@ function generateUniqueId(length) {
   return result;
 }
 
-module.exports = { generateUniqueId };
+const generateurl = () => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let url = "";
+  const charactersLength = characters.length;
+  const randomnumber = Math.random() * charactersLength;
+  for (let index = 0; index < randomnumber; index++) {
+    url += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return "https://example.com/" + url;
+};
+
+const genratehundred = async () => {
+  const urls = [];
+  for (let index = 0; index < 100; index++) {
+    const url = await generateurl();
+    const uniqueId = await generateUniqueId(4);
+    urls.push({ full: url, short: uniqueId });
+  }
+
+  return urls;
+};
+
+module.exports = { generateUniqueId, genratehundred };
